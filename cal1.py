f8366f2a7074e1a1786fb02fc69d4d58e4834e9a
AdarshGoyal <36807952+Adarshg315@users.noreply.github.com>
2019-01-31 20:13:31 +0530

    Update cal1.py

--- cal1.py
+++ cal1.py
@@ -1,27 +1,40 @@
-import time 
-import re
-import speech_recognition as sr
+import speech_recognition as sr 
 from win32com.client import Dispatch
-
+import numpy as np
 
 r = sr.Recognizer()
 
 speak = Dispatch("SAPI.SpVoice")
 speak.Volume = 100
-speak.Rate = 4
+speak.Rate = 3
 
 speak.Speak('''This is the voice calculator,Speak out the operation like 
 					   first number, operation to be performed and then the second number.
 					   For example, for 1 + 2 say one, plus, two and so on....
-					   Speak''')
+					   Speak the operation''')
 	
 while(True):
 	with sr.Microphone() as source:
 		audio1 = r.listen(source)
+		
+		
 		inputString = r.recognize_google(audio1, language= "en-IN")
+		#speak.Speak(" You said {} " + inputString)
+		
 		inputString = inputString.replace("into", "*")
+		inputString = inputString.replace("add ", "")
+		inputString = inputString.replace("product of", "")
+		inputString = inputString.replace("x", "*")
+		inputString = inputString.replace("X", "*")
+		inputString = inputString.replace("addition of", "")
+		inputString = inputString.replace("division of", "")
+		inputString = inputString.replace("from", "")
+		inputString = inputString.replace("point", ".")
+		inputString = inputString.replace("dot", ".")
 		inputString = inputString.replace("divided by", "/")
 		inputString = inputString.replace("multiplied by", "*")
+		inputString = inputString.replace("equal", "")
+		inputString = inputString.replace("=", "")
 		inputString = inputString.replace("minus", "-")
 		inputString = inputString.replace("plus", "+")
 		inputString = inputString.replace("one", "1 ")
@@ -34,11 +47,12 @@ while(True):
 		inputString = inputString.replace("eight", "8")
 		inputString = inputString.replace("nine", "9")
 		inputString = inputString.replace("ten", "10")
-
-		speak.Speak(" You said {} " + inputString)
+		inputString = inputString.replace("do", "")
+		inputString = inputString.replace("evaluate", "")
+			
 		print("You said :", inputString)
 		
-		speak.Speak("output is {} " + str(eval(inputString)))
+		speak.Speak("output is {} " + str(eval(inputString))) 
 		print("output is : ", eval(inputString))
 		
 		speak.Speak("Do you want to continue or exit")
@@ -46,8 +60,7 @@ while(True):
 		inputString = r.recognize_google(audio2, language= "en-US")
 		
 		if inputString == "continue":
-			print("You said :" , inputString,)
-			speak.Speak("now speak another operation")
+			speak.Speak("speak another operation")
 			continue
 		elif inputString == "exit":
 			print("Terminating")
